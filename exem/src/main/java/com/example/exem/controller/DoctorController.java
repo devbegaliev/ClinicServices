de89ab5cf099d469ca3fe2418d46bce50919c433
Abbos Abduhakimov <abbos9113@mail.com>
2022-05-21 20:58:08 +0500

    Update

--- exem/src/main/java/com/example/exem/controller/DoctorController.java
+++ exem/src/main/java/com/example/exem/controller/DoctorController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+
+// qale ishla
 @RestController
 @RequestMapping("/doctor")
 public class DoctorController {
